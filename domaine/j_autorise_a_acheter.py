96745e1111e3144c4e289a5a0ad71f27c548bb42
Sébastien Roccaserra <sroccaserra@yahoo.com>
2017-07-28 17:35:03 +0200

    Précise les types de retours des ports

--- domaine/j_autorise_a_acheter.py
+++ domaine/j_autorise_a_acheter.py
@@ -3,5 +3,5 @@ from abc import ABC, abstractmethod
 
 class JAutoriseAAcheter(ABC):
     @abstractmethod
-    def est_ce_que_je_peux_acheter(self):
+    def est_ce_que_je_peux_acheter(self) -> bool:
         pass
--- infrastructure/services/bitcoin_api_service.py
+++ infrastructure/services/bitcoin_api_service.py
@@ -4,7 +4,7 @@ from domaine.j_obtiens_le_cours_du_bitcoin import JObtiensLeCoursDuBitcoin
 
 
 class BitcoinApiService(JObtiensLeCoursDuBitcoin):
-    def recupere_le_cours_actuel_du_bitcoin(self):
+    def recupere_le_cours_actuel_du_bitcoin(self) -> float:
         response = requests.get('https://api.blockchain.info/stats')
         market_price_usd = response.json()['market_price_usd']
         return market_price_usd
