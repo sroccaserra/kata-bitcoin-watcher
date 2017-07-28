f39d673b53fccbea3224c8366a936f0ef8f22727
Sébastien Roccaserra <sroccaserra@yahoo.com>
2017-07-28 11:59:19 +0200

    Bootstrap le courtier de manière conforme à Flask

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
 from flask import Flask
 
-from domaine.courtier import Courtier
-from infrastructure.services.bitcoin_api_service import BitcoinApiService
-from infrastructure.services.service_registry import register_service
 from infrastructure.vues.achat_vue import AchatVue
 
 app = Flask(__name__)
-bitcoin_api_service = BitcoinApiService()
-courtier = Courtier(bitcoin_api_service)
-register_service('COURTIER', courtier)
 app.add_url_rule('/', view_func=AchatVue.as_view('achat_vue'))
--- infrastructure/vues/achat_vue.py
+++ infrastructure/vues/achat_vue.py
@@ -1,11 +1,11 @@
 from flask.views import View
 
-from infrastructure.services.service_registry import get_service
+from infrastructure.bootstrap import get_courtier
 
 
 class AchatVue(View):
     def __init__(self, courtier=None):
-        self.courtier = courtier or get_service('COURTIER')
+        self.courtier = courtier or get_courtier()
 
     def dispatch_request(self):
         je_peux_acheter = self.courtier.est_ce_que_je_peux_acheter()
