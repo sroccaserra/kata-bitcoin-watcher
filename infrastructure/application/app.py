37ebd66e6b3b9c9e59f6fa006662dd2978e212fd
Sébastien Roccaserra <sroccaserra@yahoo.com>
2017-09-20 23:53:07 +0200

    Change la route "/" en "/broker"

--- infrastructure/application/app.py
+++ infrastructure/application/app.py
@@ -5,7 +5,7 @@ from infrastructure.bootstrap import get_presentateur
 app = Flask(__name__)
 
 
-@app.route('/')
-def home():
+@app.route('/broker')
+def broker():
     reponse_formatee = get_presentateur().est_ce_que_je_peux_acheter()
     return jsonify(reponse_formatee)
--- test/test_end_to_end.py
+++ test/test_end_to_end.py
@@ -5,7 +5,7 @@ from pytest import mark
 @mark.end_to_end
 class TestE2E:
     def test_end_to_end(self):
-        response = requests.get('http://localhost:5000/')
+        response = requests.get('http://localhost:5000/broker')
 
         assert response.status_code == 200
         assert 'can_I_buy_bitcoins' in response.json()
